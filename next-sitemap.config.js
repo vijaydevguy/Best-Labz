cc854fc8658c3ba8177295e91b773f1c29f4c028
vijaydevguy <vijayakumarjob01@gmail.com>
2025-07-18 16:22:26 +0530

    seo error

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://bestlabz.vercel.app',  // ✅ Replace with your actual site URL
+  siteUrl: 'https://bestlabz.vercel.app/',  // ✅ Replace with your actual site URL
   generateRobotsTxt: true,            // ✅ Will create /robots.txt
   generateIndexSitemap: true,
   sitemapSize: 5000,
